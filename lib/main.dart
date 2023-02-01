094275146dd3fcabfd7db8fb2dfd6a397e552fe7
Diva Nafisyah <diva.nafisyah.ft21@mail.umy.ac.id>
2023-02-01 13:56:52 +0700

    create splash page

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,28 @@
+import 'package:final_project_2023/app/widgets/splash.dart';
 import 'package:flutter/material.dart';
 
 import 'package:get/get.dart';
 
 import 'app/routes/app_pages.dart';
-
 void main() {
-  runApp(
-    GetMaterialApp(
-      title: "Application",
-      initialRoute: AppPages.INITIAL,
-      getPages: AppPages.routes,
-    ),
-  );
+  runApp(Cleanin());
+}
+class Cleanin extends StatelessWidget {
+  const Cleanin({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder(
+        future: Future.delayed(Duration(seconds: 3)),
+        builder: (context, Snapshot) {
+          if (Snapshot.connectionState == ConnectionState.waiting) {
+            return SplashScreen();
+          } else 
+          return GetMaterialApp(
+            title: "Cleanin",
+            initialRoute: AppPages.INITIAL,
+            getPages: AppPages.routes,
+          );
+        }
+    );
+  }
 }
