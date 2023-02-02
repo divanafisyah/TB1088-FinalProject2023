cc8263dc50b5607b852268b93c660dfbaffabd2b
Diva Nafisyah <diva.nafisyah.ft21@mail.umy.ac.id>
2023-02-02 10:14:15 +0700

    add border in homepage

--- lib/app/modules/home/controllers/home_controller.dart
+++ lib/app/modules/home/controllers/home_controller.dart
@@ -1,20 +1,24 @@
 import 'package:get/get.dart';
 
 class HomeController extends GetxController {
+  // ignore: todo
   //TODO: Implement HomeController
 
   final count = 0.obs;
   @override
+  // ignore: unnecessary_overrides
   void onInit() {
     super.onInit();
   }
 
   @override
+  // ignore: unnecessary_overrides
   void onReady() {
     super.onReady();
   }
 
   @override
+  // ignore: unnecessary_overrides
   void onClose() {
     super.onClose();
   }
--- lib/app/modules/home/views/home_view.dart
+++ lib/app/modules/home/views/home_view.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: prefer_const_constructors
+
 import 'package:final_project_2023/theme.dart';
 import 'package:flutter/material.dart';
 
@@ -10,39 +12,68 @@ class HomeView extends GetView<HomeController> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-      centerTitle: false,
-      title: RichText(
-          text: TextSpan(
-        text: "Hai, ",
-        style: TextStyle(
-          fontSize: 18,
-        ),
-        children: [
-          TextSpan(
-            text: "Ayo Lapor Sampah",
-            style: TextStyle(
-              fontSize: 18,
-              fontWeight: FontWeight.bold,
+      appBar: AppBar(
+        centerTitle: false,
+        title: RichText(
+            text: TextSpan(
+          text: "Hai, ",
+          style: TextStyle(
+            fontSize: 18,
+          ),
+          // ignore: prefer_const_literals_to_create_immutables
+          children: [
+            TextSpan(
+              text: "Ayo Lapor Sampah",
+              style: TextStyle(
+                fontSize: 18,
+                fontWeight: FontWeight.bold,
+              ),
+            )
+          ],
+        )),
+        actions: [
+          GestureDetector(
+            onTap: () {},
+            child: Container(
+              margin: EdgeInsets.only(right: 20),
+              width: 30,
+              height: 30,
+              child: Image.asset(
+                "assets/icons/search.png",
+                fit: BoxFit.contain,
+              ),
             ),
-          )
+          ),
         ],
-      )),
-      actions: [
-        GestureDetector(
-          onTap: () {},
-          child: Container(
-            margin: EdgeInsets.only(right: 20),
-            width: 30,
-            height: 30,
-            child: Image.asset(
-              "assets/icons/search.png",
-              fit: BoxFit.contain,
-            ),
-          )
-        )
-      ]
-        )
-        );
+        backgroundColor: primaryGreen,
+        elevation: 0,
+      ),
+    );
+    // ignore: dead_code
+    Container(
+      decoration: const BoxDecoration(
+        border: Border(
+          top: BorderSide(color: Color(0xFFFFFFFF)),
+          left: BorderSide(color: Color(0xFFFFFFFF)),
+          right: BorderSide(),
+          bottom: BorderSide(),
+        ),
+      ),
+      child: Container(
+        padding: const EdgeInsets.symmetric(horizontal: 20.0, vertical: 2.0),
+        decoration: const BoxDecoration(
+          border: Border(
+            top: BorderSide(color: Color(0xFFDFDFDF)),
+            left: BorderSide(color: Color(0xFFDFDFDF)),
+            right: BorderSide(color: Color(0xFF7F7F7F)),
+            bottom: BorderSide(color: Color(0xFF7F7F7F)),
+          ),
+          color: Color(0xFFBFBFBF),
+        ),
+        child: const Text('OK',
+            textAlign: TextAlign.center,
+            style: TextStyle(color: Color(0xFF000000))),
+      ),
+    );
   }
 }
